9ca27094d3dbcef03feccf93aa13ec7e43e381e7
Leifer Mendez <leifer33@gmail.com>
2021-01-06 20:22:02 +0100

    Test

---
+++ README.md
@@ -0,0 +1,29 @@
+Instalar webpush
+`npm install web-push http-server -g`
+
+ web-push generate-vapid-keys --json
+
+
+Crear APP Angular => ng new my-app
+
+Instalar PWA
+ng add @angular/pwa 
+
+
+
+Obtener VAPID Key
+
+Solicitar Permiso
+Enviar
+
+
+
+Obtener VAPID Key
+
+{
+    "publicKey":"BMtQTXeTAMb6dirLG0o2oMENske28eSfSRJkK6VEdXH9lcH3mwfEU7cza8hNhEnJOOyacb95QOeIFaTpPdFn8Xw","privateKey":"eOLnIMyvDEhCWvwTOFax9MGbV0W8c_WTjZ_KCW1CREY"}
+
+
+Server
+Ruta
+Envio Payload
---
+++ api/server.js
@@ -0,0 +1,63 @@
+const webpush = require('web-push');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
+const app = express();
+/**
+ * Settings VAPID
+ */
+
+const vapidKeys = {
+    "publicKey": "BMtQTXeTAMb6dirLG0o2oMENske28eSfSRJkK6VEdXH9lcH3mwfEU7cza8hNhEnJOOyacb95QOeIFaTpPdFn8Xw",
+    "privateKey": "eOLnIMyvDEhCWvwTOFax9MGbV0W8c_WTjZ_KCW1CREY"
+}
+
+webpush.setVapidDetails(
+    'mailto:example@yourdomain.org',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+);
+
+const enviarNotificacion = (req, res) => {
+
+    const pushSubscription = {
+        endpoint: 'https://fcm.googleapis.com/fcm/send/cvnWI1iv1n4:APA91bEXvhI0HVrovVh82lI4HhcRKGT6dh28Bs3PpUAthvy68tz54PxZaVNj18cIOk8rK6EuAxZ3DCBdHiFM-v3y6cyD_s--JON13JP8zpaBMJE1fZUmijtzwUIst3QWxkpWgeX4VnGE',
+        keys: {
+            auth: 'H1UaF1zNA1jPlVJw6nLAGQ',
+            p256dh: 'BJIAsd0i40iAtHUinrAzNeR8UGb5HG8oEHCkcyPOda67i9UVIUhtWpD0ULEnITYX9pT8TlMTT6-ZdZFMO0-r7QE'
+        }
+    };
+
+    const payload = {
+        "notification": {
+            "title": "😄😄 Saludos",
+            "body": "Subscribete a mi canal de YOUTUBE",
+            "vibrate": [100, 50, 100],
+            "image": "https://avatars2.githubusercontent.com/u/15802366?s=460&u=ac6cc646599f2ed6c4699a74b15192a29177f85a&v=4",
+            "actions": [{
+                "action": "explore",
+                "title": "Go to the site"
+            }]
+        }
+    }
+
+    webpush.sendNotification(
+        pushSubscription,
+        JSON.stringify(payload))
+        .then(res => {
+            console.log('Enviado !!');
+        }).catch(err => {
+            console.log('Error', err);
+        })
+
+    res.send({ data: 'Se envio subscribete!!' })
+
+}
+
+app.route('/api/enviar').post(enviarNotificacion);
+
+
+const httpServer = app.listen(9000, () => {
+    console.log("HTTP Server running at http://localhost:" + httpServer.address().port);
+});
